9b1c650f1265f184c9f791dcb0edc3bd4710c209
mralawee <38766517+mralawee@users.noreply.github.com>
2018-05-15 11:36:02 -0500

    Update a.js

--- a.js
+++ a.js
@@ -17,7 +17,7 @@ var li21= $("#element_21").val();
 var li27= $("#element_27").val();
 var querystring ="&element_1="+li1+"&element_2="+li2+"&element_28="+li28+"&element_3="+li3+"&element_4="+li4+"&element_5="+li5+"&element_6="+li6+"&element_33="+li33+"&element_8="+li8+"&element_9="+li9+"&element_35="+li35+"&element_21="+li21+"&element_1="+li1+"&element_2="+li2+"&element_12="+li12+"&element_27="+li27;
 querystring = querystring.replace(/\s/g,'%20');
-var url = "https://okstate.forms-db.com/view.php?id=207204"+querystring;
+var url = "https://okstate.forms-db.com/view.php?id=209011"+querystring;
 $("#element_36").val(url);
 });
 });
