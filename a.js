8aeed6f0cb5689334318c509329e17954996995a
mralawee <38766517+mralawee@users.noreply.github.com>
2018-05-15 14:13:37 -0500

    Update a.js

--- a.js
+++ a.js
@@ -14,10 +14,19 @@ var li33a= $("#element_33_1").val();
   var li33d= $("#element_33_4").val();
   var li33e= $("#element_33_5").val();
   var li33f= $("#element_33_6").val();
-var li8= $("#element_8").val();
-var li9= $("#element_9").val();
 var li45= $("#element_45").val();
-var querystring ="&element_1_1="+li1a+"&element_1_2="+li1b+"&element_2="+li2+"&element_28="+li28+"&element_31="+li31+"&element_43="+li43+"&element_4="+li4+"&element_5="+li5+"&element_44="+li44+"&element_33_1="+li33a+"&element_33_3="+li33c+"&element_33_4="+li33d+"&element_33_5="+li33e+"&element_33_6="+li33f+"&element_8="+li8+"&element_9="+li9+"&element_45="+li45;
+ 
+ var mm8=$("#element_8_1").val();
+var dd8=$("#element_8_2").val();
+var yy8=$("#element_8_3").val();
+var li8=mm8+"/"+dd8+"/"+yy8;
+ 
+ var mm9=$("#element_9_1").val();
+var dd9=$("#element_9_2").val();
+var yy9=$("#element_9_3").val();
+var li9=mm9+"/"+dd9+"/"+yy9;
+ 
+var querystring ="&element_1_1="+li1a+"&element_1_2="+li1b+"&element_2="+li2+"&element_28="+li28+"&element_31="+li31+"&element_43="+li43+"&element_4="+li4+"&element_5="+li5+"&element_44="+li44+"&element_33_1="+li33a+"&element_33_3="+li33c+"&element_33_4="+li33d+"&element_33_5="+li33e+"&element_33_6="+li33f+"&element_50="+li8+"&element_51="+li9+"&element_45="+li45;
 querystring = querystring.replace(/\s/g,'%20');
 var url = "https://okstate.forms-db.com/view.php?id=209011"+querystring;
 $("#element_46").val(url);
