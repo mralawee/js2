9d0f3c83a1097430c7f197ae011bdf0dc8904c0e
mralawee <38766517+mralawee@users.noreply.github.com>
2018-05-17 16:47:17 -0500

    Update htmpdf.js

--- htmpdf.js
+++ htmpdf.js
@@ -1,26 +1,4 @@
-	var oHead = document.getElementsByTagName('HEAD').item(0);
-var oScript= document.createElement("script");
-oScript.type = "text/javascript";
-oScript.src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js";
-oHead.appendChild( oScript);
-
-var oHead1 = document.getElementsByTagName('HEAD').item(0);
-var oScript1= document.createElement("script");
-oScript1.type = "text/javascript";
-oScript1.src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.2.61/jspdf.min.js";
-oHead1.appendChild( oScript1);
-
 $("#submit_form").click(function () {
-  var doc = new jsPDF();
-var specialElementHandlers = {
-    '#form_container': function (element, renderer) {
-        return true;
-    }
-};
-	
-    doc.fromHTML($('#form_container').html(), 15, 15, {
-        'width': 170,
-            'elementHandlers': specialElementHandlers
-    });
-    doc.save('sample-file.pdf');
+  
+	$("#download").setAttribute("#download", "image3")
 });
