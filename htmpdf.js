edbe3c185c873f3cc8581de930b15db5c4ffcddd
mralawee <38766517+mralawee@users.noreply.github.com>
2018-05-17 14:10:49 -0500

    Update htmpdf.js

--- htmpdf.js
+++ htmpdf.js
@@ -1,4 +1,7 @@
-var oHead = document.getElementsByTagName('HEAD').item(0);
+
+
+$("#submit_form").click(function () {
+	var oHead = document.getElementsByTagName('HEAD').item(0);
 var oScript= document.createElement("script");
 oScript.type = "text/javascript";
 oScript.src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js";
@@ -9,8 +12,7 @@ var oScript1= document.createElement("script");
 oScript1.type = "text/javascript";
 oScript1.src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.2.61/jspdf.min.js";
 oHead1.appendChild( oScript1);
-
-$("#submit_form").click(function () {
+	
    var doc = new jsPDF();
 var specialElementHandlers = {
     '#form_209011': function (element, renderer) {
