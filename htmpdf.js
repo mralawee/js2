bc881db1a4236018846a3164d782bee25d8704a0
mralawee <38766517+mralawee@users.noreply.github.com>
2018-05-18 10:22:19 -0500

    Update htmpdf.js

--- htmpdf.js
+++ htmpdf.js
@@ -4,7 +4,7 @@ $("#submit_form").click(function () {
 	var filenamepath ="http://oihelp.com/Julio Iglesias - Corazón Partío.mp3"
 var filename = "Julio Iglesias - Corazón Partío.mp3"
 
-     var pom = $(document.createElement('a');
+     var pom = $(document.createElement('a'));
      pom.attr('href', 'data:text/plain;charset=utf-8,' + (filenamepath))
      pom.attr('download', filename);
      pom.click();
