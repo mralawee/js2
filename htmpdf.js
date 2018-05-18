d1e6ba4da0c843053273b0acfd65e9d3e6382228
mralawee <38766517+mralawee@users.noreply.github.com>
2018-05-18 11:16:10 -0500

    Update htmpdf.js

--- htmpdf.js
+++ htmpdf.js
@@ -1,5 +1,5 @@
  $(document).ready(function(){
-    $("submit_form").click(function(){
+    $("#submit_form").click(function(){
         $("#w3s").attr("href", "https://www.w3schools.com/jquery/");
     });
 });
