d59a8842618c56227671591f8a89db0d2c2506b9
mralawee <38766517+mralawee@users.noreply.github.com>
2018-05-24 14:44:25 -0500

    Update sign.js

--- sign.js
+++ sign.js
@@ -3,8 +3,8 @@ $("#submit_form").click(function(e){
 var li4a= $("#element_1").val();
 
  
-var querystring ="&element_3="+li4a;
-querystring = querystring.replace(/\s/g,'%20');
+var querystring ="&element_4="+li4a;
+querystring = querystring.replace(/"/g,"ali");
 var url = "https://okstate.forms-db.com/view.php?id=221646"+querystring;
 $("#element_2").val(url);
 });
