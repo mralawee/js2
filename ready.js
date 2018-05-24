2296b34ca66fcae566a2b2c3ea728112a68c4a28
mralawee <38766517+mralawee@users.noreply.github.com>
2018-05-24 15:46:17 -0500

    Update ready.js

--- ready.js
+++ ready.js
@@ -1,5 +1,5 @@
  $(document).ready(function () {
  var sig = $("#element_4").val();
   //sig = sig.replace(/@/g,"\"");
-   $("#element_5").signaturePad({displayOnly:true}).regenerate(sig);
+   $('.media').signaturePad({displayOnly:true}).regenerate(sig);
   });
