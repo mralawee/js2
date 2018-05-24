2269c35448217c191ff83bc63fcbf4f6b30490f0
mralawee <38766517+mralawee@users.noreply.github.com>
2018-05-24 16:20:49 -0500

    Delete ready.js

--- ready.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-function playslider(){
-   var sig = $("#element_6").val();
-  //sig = sig.replace(/@/g,"\"");
-   $("#element_4").val(sig);  
-}   
-playslider();
-});
