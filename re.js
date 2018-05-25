905f59361ce31743849cc8e2c8d39ae7e9af0006
mralawee <38766517+mralawee@users.noreply.github.com>
2018-05-25 10:12:13 -0500

    Update re.js

--- re.js
+++ re.js
@@ -1,10 +1,11 @@
- $(document).ready(function () {
-  $(window).scroll(function() {
- var profsign= $("#element_42").val();
+$(document).ready(function () {
+   $("#img1").click(function () {
+  var profsign= $("#element_42").val();
  var stusign= $("#element_43").val();
-   profsign = profsign.replace(/@/g, '"');
+       profsign = profsign.replace(/@/g, '"');
  stusign = stusign.replace(/@/g, '"');
-    $('.sigPad').signaturePad({displayOnly:true}).regenerate(profsign);
+       $('.sigPad').signaturePad({displayOnly:true}).regenerate(profsign);
  $('.ssigPad').signaturePad({displayOnly:true}).regenerate(stusign);
-});
-   });
+     });
+ 
+  });
