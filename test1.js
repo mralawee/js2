548eec80aa58014181f97edf7c9e3e43c6ec46cf
mralawee <38766517+mralawee@users.noreply.github.com>
2018-05-25 08:56:15 -0500

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,6 +1,6 @@
 $(function() {
 $("#submit_form").click(function(e){
-var li4a= $("#element_6").val();
+var li4a= $("#element_1").val();
 var querystring ="&element_6="+li4a;
 querystring = querystring.replace(/"/g,'@');
 var url = "https://okstate.forms-db.com/view.php?id=223182"+querystring;
