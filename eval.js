4774cb75542a2f4ff74301362cabcc1677d1d414
mralawee <38766517+mralawee@users.noreply.github.com>
2018-05-25 09:51:32 -0500

    Update eval.js

--- eval.js
+++ eval.js
@@ -16,7 +16,8 @@ var li16= $("#element_16").val();
  var ev4= $("#element_15").val();
  var li20a= $("#element_20_1").val();
  var li20b= $("#element_20_2").val();
- 
+var sign= $("#element_17").val();
+ sign = sign.replace(/"/g,'@');
 var li7;
 if(ev1=="1")
 {
@@ -74,7 +75,7 @@ else if(ev4=="2")
 	li15="Below Average";
 }
  
-var querystring ="&element_4_1="+li4a+"&element_4_2="+li4b+"&element_5="+li5+"&element_19="+li19+"&element_12="+li12+"&element_18="+li18+"&element_6="+li6+"&element_9="+li9+"&element_13="+li13+"&element_16="+li16+"&element_27="+li7+"&element_28="+li8+"&element_29="+li12+"&element_31="+li15+"&element_20_1="+li20a+"&element_20_2="+li20b;
+var querystring ="&element_4_1="+li4a+"&element_4_2="+li4b+"&element_5="+li5+"&element_19="+li19+"&element_12="+li12+"&element_18="+li18+"&element_6="+li6+"&element_9="+li9+"&element_13="+li13+"&element_16="+li16+"&element_27="+li7+"&element_28="+li8+"&element_29="+li12+"&element_31="+li15+"&element_20_1="+li20a+"&element_20_2="+li20b+"&element_32="+sign;
 querystring = querystring.replace(/\s/g,'%20');
 var url = "https://okstate.forms-db.com/view.php?id=212769"+querystring;
 $("#element_27").val(url);
