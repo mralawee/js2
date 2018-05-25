02a7895f24ab972ef667bdb2d12e2317d5166e89
mralawee <38766517+mralawee@users.noreply.github.com>
2018-05-25 11:06:40 -0500

    Update evall.js

--- evall.js
+++ evall.js
@@ -49,6 +49,6 @@ var stusign=$("#element_17").val();
 var querystring ="&element_4_1="+li4a+"&element_4_2="+li4b+"&element_5="+li5+"&element_19="+li19+"&element_12="+li12+"&element_18="+li18+"&element_6="+li6+"&element_9="+li9+"&element_13="+li13+"&element_16="+li16+"&element_27="+li27+"&element_28="+li28+"&element_29="+li29+"&element_31="+li31+"&element_20_1="+li20a+"&element_20_2="+li20b+"&element_32="+li23+"&element_33="+li30+"&element_22="+li22+"&element_25="+li25+"&element_26="+li26+"&element_42="+profsign+"&element_43="+stusign;
 querystring = querystring.replace(/\s/g,'%20');
 var url = "https://okstate.forms-db.com/view.php?id=213202"+querystring;
-$("#element_33").val(url);
+$("#element_32").val(url);
 });
 });
