b7bfb3b616bae9aaf829a3a057d34c313d2b683a
mralawee <38766517+mralawee@users.noreply.github.com>
2018-05-25 08:48:06 -0500

    Update re.js

--- re.js
+++ re.js
@@ -1,6 +1,7 @@
  $(document).ready(function () {
   $("#img1").click(function () {
      var sig= $("#element_1").val();
+   sig = sig.replace(/@/g, '"');
     $('.sigPad').signaturePad({displayOnly:true}).regenerate(sig);
     });
   });
