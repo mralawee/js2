fad8d58e7a656bf009cfbb591411ef85e75bc79d
mralawee <38766517+mralawee@users.noreply.github.com>
2018-06-05 11:47:28 -0500

    Update internname.js

--- internname.js
+++ internname.js
@@ -1,6 +1,6 @@
 $("#submit_primary").click(function(){
 var name1= $("#element_4").val();
  // var name2= $("#element_1_2").val();
-$("#element_5").val(name1);
+$("#element_3").val(name1);
  // $("#element_2_2").val(name2);
 });
