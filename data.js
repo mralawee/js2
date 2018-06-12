7c3b9ccf1c54048046eddfa8e4d6fc0254070f52
mralawee <38766517+mralawee@users.noreply.github.com>
2018-06-12 10:51:53 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,36 @@
 $(function(){
-var name= $("#element_2").val();
+ var name1= $("#element_1_1").val();
+var name2= $("#element_1_2").val();
+ var sevis= $("#element_2").val();
+ var cwid= $("#element_28").val();
+ var email= $("#element_31").val();
+ var visa= $("#element_43").val();
+ var clss= $("#element_4").val();
+ var major= $("#element_5").val();
+ var dep= $("#element_50").val();
+ var ret= $("#element_51").val();
+ var tcoc= $("#element_45").val();
+ var street= $("#element_33_1").val();
+ var city= $("#element_33_3").val();
+ var state= $("#element_33_4").val();
+ var zip= $("#element_33_5").val();
+ var coc= $("#element_33_6").val();
  $("#submit_form").click(function(e){
-$("#element_2").val(name);
+  $("#element_2").val(sevis);
+  $("#element_1_1").val(name1);
+  $("#element_1_2").val(name2);
+  $("#element_28").val(cwid);
+  $("#element_31").val(email);
+  $("#element_43").val(visa);
+  $("#element_4").val(clss);
+  $("#element_5").val(major);
+  $("#element_50").val(dep);
+  $("#element_51").val(ret);
+  $("#element_45").val(tcoc);
+  $("#element_33_1").val(street);
+  $("#element_33_3").val(city);
+  $("#element_33_4").val(state);
+  $("#element_33_5").val(zip);
+  $("#element_33_6").val(coc);
 });
   });
