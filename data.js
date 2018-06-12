7a6805e29cd696736eb6f5abac9b6571b1af5c78
mralawee <38766517+mralawee@users.noreply.github.com>
2018-06-12 10:28:03 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 $(function(){
 var name= $("#element_2").val();
-  $("#submit_form").click(function(e){
-$("#element_2").text(name);
+ $("#submit_form").click(function(e){
+$("#element_2").val(name);
 });
-});
+  });
