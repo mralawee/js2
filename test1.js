e4c46631575798f7cf7dcd7a780f477ff965fa15
mralawee <38766517+mralawee@users.noreply.github.com>
2018-06-14 09:02:59 -0500

    Delete test1.js

--- test1.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-$("#submit_form").click(function(e){
-var li4a= $("#element_1").val();
-var querystring ="&element_6="+li4a;
-querystring = querystring.replace(/"/g,'@');
-var url = "https://okstate.forms-db.com/view.php?id=223182"+querystring;
-$("#element_6").val(url);
-});
-});
