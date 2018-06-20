3f05ae5fdbb4d6c08d28c02a25126b0d38270f72
mralawee <38766517+mralawee@users.noreply.github.com>
2018-06-20 15:59:08 -0500

    Update sup-completion-iss.js

--- sup-completion-iss.js
+++ sup-completion-iss.js
@@ -1,7 +1,7 @@
 $(function() {
   $("#element_24").each(function(){
 var querystring = $("#element_24").val();
-querystring = querystring.replace(/\-/g,'\n');
+querystring = querystring.replace(/\--/g,'\n');
 $("#element_24").val(querystring);
 });
 });
