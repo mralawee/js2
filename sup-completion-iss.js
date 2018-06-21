30d9e73fcae501c5cb2e69090a5c50b813018867
mralawee <38766517+mralawee@users.noreply.github.com>
2018-06-21 13:45:48 -0500

    Update sup-completion-iss.js

--- sup-completion-iss.js
+++ sup-completion-iss.js
@@ -1,23 +1,7 @@
 $(function() {
-  
-			
-
   $("#element_24").each(function(){
 var querystring = $("#element_24").val();
 querystring = querystring.replace(/\--/g,'\n');
 $("#element_24").val(querystring);
-});
-	
-			var sigpad_options = {
-				drawOnly : true,
-				displayOnly: true,
-				bgColour: '#fff',
-				penColour: '#000',
-				validateFields: false
-			};
-			var sigpad_data = $("#element_26").val();
-	 sigpad_data = sigpad_data.replace(/-/g, '"');
-		$("#element_26").val(sigpad_data);
-			$('#mf_sigpad').signaturePad(sigpad_options).regenerate(sigpad_data);
-		
+});		
 });
