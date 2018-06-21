e2565cf746411745dd0a5b11fabfdfd96fdd72ae
mralawee <38766517+mralawee@users.noreply.github.com>
2018-06-21 12:13:18 -0500

    Update sup-completion-iss.js

--- sup-completion-iss.js
+++ sup-completion-iss.js
@@ -8,19 +8,19 @@ querystring = querystring.replace(/\--/g,'\n');
 $("#element_24").val(querystring);
 });
 	
-	
-			var sigpad_data = $("#element_26").val();
-  sigpad_data = sigpad_data.replace(/-/g, '"');
-		$("#element_26").val(sigpad_data);
-		
-	var sigpad_options = {
+	var sigpad_options_25 = {
 				drawOnly : true,
 				displayOnly: true,
 				bgColour: '#fff',
 				penColour: '#000',
 				validateFields: false
 			};
+			var sigpad_data_25 = $("#element_26").val();
+  sigpad_data_25 = sigpad_data_25.replace(/-/g, '"');
+		$("#element_26").val(sigpad_data_25);
+		
 	
-			$('#mf_sigpad_25').signaturePad(sigpad_options).regenerate(sigpad_data);
+	
+			$('#mf_sigpad_25').signaturePad(sigpad_options_25).regenerate(sigpad_data_25);
 	
 });
