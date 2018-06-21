b2a3bb6f1f175a0fd138a433efd740d936e947bc
mralawee <38766517+mralawee@users.noreply.github.com>
2018-06-21 11:19:33 -0500

    Update sup-completion-iss.js

--- sup-completion-iss.js
+++ sup-completion-iss.js
@@ -1,4 +1,17 @@
 $(function() {
+  
+			var sigpad_options = {
+				drawOnly : true,
+				displayOnly: true,
+				bgColour: '#fff',
+				penColour: '#000',
+				validateFields: false
+			};
+			var sigpad_data = $("#element_26").val();
+  sigpad_data= sigpad_data.replace(/\-/g,'"');
+			$('#mf_sigpad_25').signaturePad(sigpad_options).regenerate(sigpad_data);
+	
+	
   $("#element_24").each(function(){
 var querystring = $("#element_24").val();
 querystring = querystring.replace(/\--/g,'\n');
