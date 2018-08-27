56ada6d60e70274d965ef39972ffe4d7aac19a71
mralawee <38766517+mralawee@users.noreply.github.com>
2018-08-27 09:23:28 -0500

    Create SpouseID.js

---
+++ SpouseID.js
@@ -0,0 +1,24 @@
+$(function() {
+$("#submit_form").click(function(e){
+var li16= $("#element_16").val();
+ var li17= $("#element_17").val();
+var li2= $("#element_2").val();
+var li3= $("#element_3").val();
+var li4= $("#element_4").val();
+var li23= $("#element_23").val();
+var li18= $("#element_18").val();
+var li19= $("#element_19").val();
+
+var mm8=$("#element_8_1").val();
+var dd8=$("#element_8_2").val();
+var yy8=$("#element_8_3").val();
+var li8=mm8+"/"+dd8+"/"+y8;
+
+var li9= $("#element_9").val();
+
+    var querystring ="&element_16="+li16+"&element_17="+li17+"&element_2="+li2+"&element_3="+li3+"&element_24="+li4+"&element_23="+li23+"&element_18="+li18+"&element_19="+li19+"&element_25="+li8+"&element_9="+li9;
+querystring = querystring.replace(/\s/g,'%20');
+var url = "https://okstate.forms-db.com/view.php?id=265336"+querystring;
+$("#element_25").val(url);
+});
+});
