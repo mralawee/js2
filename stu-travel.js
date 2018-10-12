63ec8924345f74d05c258a41d36ef84632826d70
mralawee <38766517+mralawee@users.noreply.github.com>
2018-10-12 10:08:22 -0500

    Update stu-travel.js

--- stu-travel.js
+++ stu-travel.js
@@ -32,7 +32,7 @@ var dd7=$("#element_47_2").val();
 var yy7=$("#element_47_3").val();
 var li7=mm7+"/"+dd7+"/"+yy7;
  
-var querystring ="&element_1_1="+li1a+"&element_1_2="+li1b+"&element_2="+li2+"&element_28="+li28+"&element_31="+li31+"&element_43="+li43+"&element_4="+li4+"&element_5="+li5+"&element_44="+li44+"&element_33_1="+li33a+"&element_33_3="+li33c+"&element_33_4="+li33d+"&element_33_5="+li33e+"&element_33_6="+li33f+"&element_50="+li8+"&element_51="+li9+"&element_45="+li45+"&element_53="+li7+"&element_57="+li53;
+var querystring ="&element_1_1="+li1a+"&element_1_2="+li1b+"&element_2="+li2+"&element_28="+li28+"&element_31="+li31+"&element_43="+li43+"&element_4="+li4+"&element_5="+li5+"&element_44="+li44+"&element_33_1="+li33a+"&element_33_3="+li33c+"&element_33_4="+li33d+"&element_33_5="+li33e+"&element_33_6="+li33f+"&element_50="+li8+"&element_51="+li9+"&element_45="+li45+"&element_53="+li7+"&element_59="+li53;
 querystring = querystring.replace(/\s/g,'%20');
 var url = "https://okstate.forms-db.com/view.php?id=209011"+querystring;
 $("#element_46").val(url);
