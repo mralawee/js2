2a8cc3ec42ff8ebc8fd575d543dfba28e262e95c
mralawee <38766517+mralawee@users.noreply.github.com>
2018-12-18 10:55:20 -0600

    Update barcode-reload.js

--- barcode-reload.js
+++ barcode-reload.js
@@ -2,8 +2,6 @@ $(function() {
 $("#element_1").focus();
 $( "#form_314034" ).submit(function( event ) {
   
-var url = "https://okstate.forms-db.com/view.php?id=314034";    
-$(location).attr('href',url);
-
 });
+  location.reload();
 });
