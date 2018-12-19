2fd787fc66b87823ec0328b05734f52de7a5f198
mralawee <38766517+mralawee@users.noreply.github.com>
2018-12-19 08:30:02 -0600

    Update barcode-reload.js

--- barcode-reload.js
+++ barcode-reload.js
@@ -1,3 +1,11 @@
-$( "#element_1" ).change(function() {
-  alert( "Handler for .change() called." );
+$(function() {
+  $("#element_1").focus();
+ var le = $("#element_1").val().replace(/ /g,'').length
+
+ if (le == 9) {
+$( "#form_314034" ).submit();
+  window.history.back();
+   $("#element_1").val('');
+ }
+});
 });
