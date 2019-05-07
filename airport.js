05b833b0b877cff1608d0667a6cefd0e5957f976
mralawee <38766517+mralawee@users.noreply.github.com>
2019-05-07 11:32:01 -0500

    Update airport.js

--- airport.js
+++ airport.js
@@ -2,7 +2,7 @@ $(function() {
   $("#element_2").datepicker({
     beforeShowDay: function(date) {
       var today = new Date().getDate(),
-        tomorrow = today + 1;
+        tomorrow = today + 2;
       // date is not today, tomorrow, or week-end
       if ((today == date.getDate() || tomorrow == date.getDate()) || date.getDay() % 6 == 0) {
         return [false, "CSSclass", "disabled"];
