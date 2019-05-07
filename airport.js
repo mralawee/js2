6b8898d67fa80f4ed1af8b79db90554d9eb380d6
mralawee <38766517+mralawee@users.noreply.github.com>
2019-05-07 11:31:47 -0500

    Update airport.js

--- airport.js
+++ airport.js
@@ -1,4 +1,14 @@
-$(function()
-{
-    $("element_4_3").val("Oklahoma");
+$(function() {
+  $("#element_2").datepicker({
+    beforeShowDay: function(date) {
+      var today = new Date().getDate(),
+        tomorrow = today + 1;
+      // date is not today, tomorrow, or week-end
+      if ((today == date.getDate() || tomorrow == date.getDate()) || date.getDay() % 6 == 0) {
+        return [false, "CSSclass", "disabled"];
+      } else {
+        return [true, '', ''];
+      }
+    }
+  })
 });
