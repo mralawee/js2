c5aa2836539bc720dcce6c07c80cc02d546ec4a4
mralawee <38766517+mralawee@users.noreply.github.com>
2020-08-04 13:45:02 -0500

    Update workpermitform.js

--- workpermitform.js
+++ workpermitform.js
@@ -26,6 +26,8 @@ var li6= $("#element_6").val();
 var li7= $("#element_7").val();
 
 var li8= $("#element_8").val();
+	
+var li21= $("#element_21").val();
 
 var stat=$("input[name='element_9']:checked").val();
 var li9;
@@ -60,7 +62,7 @@ var li13= $("#element_13").val();
 	
 var li35= $("#element_20").val();
  
-var querystring ="&element_1_1="+li1a+"&element_1_2="+li1b+"&element_2="+li2+"&element_3_1="+li3a+"&element_3_3="+li3c+"&element_3_4="+li3d+"&element_3_5="+li3e+"&element_3_6="+li3f+"&element_20="+li4+"&element_21="+li5+"&element_6="+li6+"&element_7="+li7+"&element_8="+li8+"&element_22="+li9+"&element_10="+li10+"&element_11="+li11+"&element_12="+li12+"&element_13="+li13+"&element_35="+li35;
+var querystring ="&element_1_1="+li1a+"&element_1_2="+li1b+"&element_2="+li2+"&element_3_1="+li3a+"&element_3_3="+li3c+"&element_3_4="+li3d+"&element_3_5="+li3e+"&element_3_6="+li3f+"&element_20="+li4+"&element_21="+li5+"&element_6="+li6+"&element_7="+li7+"&element_8="+li8+"&element_22="+li9+"&element_10="+li10+"&element_11="+li11+"&element_12="+li12+"&element_13="+li13+"&element_35="+li35+"&element_38="+li21;
 querystring = querystring.replace(/\s/g,'%20');
 var url = "https://okstate.forms-db.com/view.php?id=587125"+querystring;
 $("#element_19").val(url);
